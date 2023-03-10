2867d3ab3543cacc0e400ba4f2abb2f1e371a5b7
D3mon5oul06 <98986072+D3mon5oul06@users.noreply.github.com>
2023-03-10 10:26:05 -0600

    Creacion mutation

--- numeros/schema.py
+++ numeros/schema.py
@@ -10,7 +10,57 @@ class NumeroType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    Numero = graphene.List(NumeroType)
+    numeros = graphene.List(NumeroType)
 
-    def resolve_Numero(self, info, **kwargs):
+    def resolve_numeros(self, info, **kwargs):
         return Numero.objects.all()
+
+# ...code
+#1
+class CreateNumero(graphene.Mutation):
+    id = graphene.Int()
+    titulo = graphene.String()
+    paginas = graphene.Int()
+    autor = graphene.String()
+    clasificacion = graphene.Int()
+    pais = graphene.String()
+    genero = graphene.String()
+    capitulos = graphene.Int()
+    serializacion = graphene.Int()
+    precio = graphene.Int()
+
+    #2
+    class Arguments:
+       id = graphene.Int()
+       titulo = graphene.String()
+       paginas = graphene.Int()
+       autor = graphene.String()
+       clasificacion = graphene.Int()
+       pais = graphene.String()
+       genero = graphene.String()
+       capitulos = graphene.Int()
+       serializacion = graphene.Int()
+       precio = graphene.Int()
+
+    #3
+    def mutate(self, info, titulo, paginas, autor, clasificacion, pais, genero, capitulos, serializacion, precio ):
+        numero = Numero(titulo = titulo, paginas = paginas, autor = autor, clasificacion = clasificacion, pais = pais, genero = genero, capitulos = capitulos, serializacion = serializacion, precio = precio)
+        numero.save()
+
+        return CreateNumero(
+            id=numero.id,
+            titulo = numero.titulo,
+            paginas =  numero.paginas,
+            autor = numero.autor,
+            clasificacion = numero.clasificacion,
+            pais = numero.pais,
+            genero = numero.genero,
+            capitulos = numero.capitulos,
+            serializacion = numero.serializacion,
+            precio = numero.precio,
+        )
+
+
+#4
+class Mutation(graphene.ObjectType):
+    create_Numero = CreateNumero.Field()
